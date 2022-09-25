80ce3960918ea3d3a258b7776fe5fab12d16ef85
dpisati <43629737+dpisati@users.noreply.github.com>
2022-09-26 10:41:41 +1300

    end

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,8 @@ import "express-async-errors";
 import { router } from "./routes";
 import "dotenv/config";
 import { AppError } from "@shared/errors/AppError";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 import createConnection from "@shared/infra/typeorm";
 import "@shared/container";
@@ -20,6 +22,18 @@ const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -29,6 +43,13 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(function onError(err, req, res, next) {
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
